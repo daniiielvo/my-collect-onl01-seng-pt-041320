1dbd020e8b130fe9c250975fad8023e9e03f94a8
Daniel Vo <daniiielvo@gmail.com>
2020-04-22 20:52:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(array)
   counter = 0 
   empty_array = []
   while array < array.length 
-  empty_array<< yield(array[counter])
   array.collect do |array| (" , ")
+  empty_array<< yield(array[counter])
   
   
 end 
